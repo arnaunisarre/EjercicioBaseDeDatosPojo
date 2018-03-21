00385305ab841c7521606c032644dc8bbe9d128e
lcadena <36576978+lcadena@users.noreply.github.com>
2018-03-21 15:37:38 +0100

    2Commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        System.out.println("Hello World!");
-
-
         Session s = Factory.openSession();
+
+        Employee e4 = (Employee)s.save(Employee.class, "Lucas", 1, 900);
+        Employee e5 = (Employee)s.save(Employee.class, "María", 2, 900);
 
         Employee e1 = (Employee)s.get(Employee.class, 7);
         Employee e2 = (Employee)s.get(Employee.class, 7);
+
+        Employee e3 = (Employee)s.delete(Employee.class, 2);
+
+        Employee e6 = (Employee)s.update(Employee.class, "María", 2);
 
         Department d = (Department)s.get(Department.class, 99);
 
--- src/Session.java
+++ src/Session.java
@@ -1,32 +1,31 @@
 public class Session {
 
-public  Session(){
-
-}
-public  void save(Class c) {
-
-
-
-
-
-
-   //String query1 = "INSERT INTO"+ c.getSimpleName() +"("+ id + "," + name "," + salary + ")";
-
-
-}
-
-public  void update(){
-
+    public  Session(){
 
     }
-    public  Object  get(Class c, int id){
-       String query =" SELECT * FROM "+c.getSimpleName()+" WHERE id = ?";
-       System.out.println(query);
+    public Object save(Class c, String name, int id, int salary) {
+        String query1 = "INSERT INTO " + c.getSimpleName()+ "("+ id +"," + name + ","+ salary +")VALUES (?, ?, ?)";
+        System.out.println(query1);
+       //String query1 = "INSERT INTO"+ c.getSimpleName() +"("+ id + "," + name "," + salary + ")";
+        return null;
+
+    }
+
+    public  Object update(Class c, String name, int id){
+        String query3 = "UPDATE * FROM " +c.getSimpleName()+ "WHERE name = ? AND id = ?";
         return null;
     }
-    public  void delete(){
 
+    public  Object get(Class c, int id){
+       String query =" SELECT * FROM "+c.getSimpleName()+" WHERE id = ?";
+       System.out.println(query);
+       return null;
+    }
 
+    public Object delete (Class c, int id) {
+        String query2 = "DELETE * FROM " +c.getSimpleName()+ " WHERE id =?";
+        System.out.println(query2);
+        return null;
     }
 
 
